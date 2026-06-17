c067497bc86962fa235346ecc26a67c07a7c071d
AKSHAY89350 <akshay89350@gmail.com>
2026-06-17 23:26:12 +0530

    Implement identity service authentication and JWT setup

--- SmartPMS.IdentityService/Data/IdentityDbContext.cs
+++ SmartPMS.IdentityService/Data/IdentityDbContext.cs
@@ -1,6 +1,51 @@
-﻿namespace SmartPMS.IdentityService.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SmartPMS.IdentityService.Models;
+
+namespace SmartPMS.IdentityService.Data
 {
-    public class IdentityDbContext
+    public class IdentityDbContext : DbContext
     {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users => Set<AppUser>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.EmployeeCode)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                entity.HasIndex(x => x.EmployeeCode)
+                    .IsUnique();
+
+                entity.Property(x => x.FullName)
+                    .HasMaxLength(150)
+                    .IsRequired();
+
+                entity.Property(x => x.Email)
+                    .HasMaxLength(150)
+                    .IsRequired();
+
+                entity.HasIndex(x => x.Email)
+                    .IsUnique();
+
+                entity.Property(x => x.PasswordHash)
+                    .IsRequired();
+
+                entity.Property(x => x.Role)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                entity.Property(x => x.IsActive)
+                    .HasDefaultValue(true);
+            });
+        }
     }
 }
