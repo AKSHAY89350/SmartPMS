4e9c4ecc2ac29023da5c797c8e6a4203ac2791e4
AKSHAY89350 <akshay89350@gmail.com>
2026-06-17 23:26:12 +0530

    Implement identity service authentication and JWT setup

--- SmartPMS.IdentityService/Program.cs
+++ SmartPMS.IdentityService/Program.cs
@@ -1,9 +1,68 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SmartPMS.IdentityService.Data;
+using SmartPMS.IdentityService.Services;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+var jwtKey = builder.Configuration["Jwt:Key"];
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+var jwtAudience = builder.Configuration["Jwt:Audience"];
+
+if (string.IsNullOrWhiteSpace(jwtKey))
+{
+    throw new Exception("JWT Key is missing in appsettings.json");
+}
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = jwtIssuer,
+        ValidAudience = jwtAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("SmartPMSCors", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -13,9 +72,12 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
+app.UseCors("SmartPMSCors");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- SmartPMS.IdentityService/Services/IAuthService.cs
+++ SmartPMS.IdentityService/Services/IAuthService.cs
@@ -1,6 +1,12 @@
-﻿namespace SmartPMS.IdentityService.Services
+﻿using SmartPMS.IdentityService.DTOs;
+using SmartPMS.Shared.Common;
+
+namespace SmartPMS.IdentityService.Services
 {
     public interface IAuthService
     {
+        Task<ApiResponse<AuthResponseDto>> RegisterAsync(RegisterRequestDto request);
+
+        Task<ApiResponse<AuthResponseDto>> LoginAsync(LoginRequestDto request);
     }
 }
